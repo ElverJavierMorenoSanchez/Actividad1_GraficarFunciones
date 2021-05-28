8f48736e8c78f8f250d9811459ac5db2f985c436
Javier Snz <ejmoreno6@espe.edu.ec>
2021-05-27 21:34:21 -0500

    Programa que grafica dos funciones, está casi terminado pero falta comentar el código

---
+++ main.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('seaborn')
+
+#Sistema lineal
+x = np.linspace(-15,15,1000)
+
+def f(x):
+    return x**2 - x + 1
+
+def g(x):
+    return 2/(x-1)
+
+def graficar():
+
+    fig, ax = plt.subplots()
+    ax.plot(x,f(x))
+    ax.plot(x,g(x))
+
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+
+    ax.grid(True, linestyle = '-')
+
+    ax.annotate("", xy = (xmax,0), xytext = (xmin,0),
+            arrowprops=dict(color = 'gray', width = 1.5, headwidth = 8, headlength = 10))
+
+    ax.annotate("", xy = (0,ymax), xytext = (0,ymin),
+            arrowprops=dict(color = 'gray', width = 1.5, headwidth = 8, headlength = 10))
+
+    plt.show()
+
+#Mostrar Resultados
+
+graficar()

1389170e8c2b4cc66893f42eac088be35b8896f4
Javier Snz <ejmoreno6@espe.edu.ec>
2021-06-02 15:49:41 -0500

    Se agregaron comentarios al código

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+#Importamos las librerias
 import matplotlib.pyplot as plt
 import numpy as np
 
 plt.style.use('seaborn')
 
 #Sistema lineal
-x = np.linspace(-15,15,1000)
+x = np.linspace(-15,15,1000) #Se crea el espacio que tendra el sistema lineal
 
+#Primera Función
 def f(x):
     return x**2 - x + 1
-
+#Segunda función
 def g(x):
     return 2/(x-1)
 
+#Funcion que grafica las funciones
 def graficar():
 
-    fig, ax = plt.subplots()
+    #Codigo principal
+    fig, ax = plt.subplots() #Se encarga de graficar el plano
     ax.plot(x,f(x))
     ax.plot(x,g(x))
 
+    #### Codigo que ayuda a la estetica de la grafica final ####
     xmin, xmax = ax.get_xlim()
     ymin, ymax = ax.get_ylim()
 
@@ -28,10 +33,10 @@ def graficar():
 
     ax.annotate("", xy = (0,ymax), xytext = (0,ymin),
             arrowprops=dict(color = 'gray', width = 1.5, headwidth = 8, headlength = 10))
+    ################### Fin Codigo #########################
 
-    plt.show()
+    plt.show() #Muestra las graficas
 
 #Mostrar Resultados
-
 graficar()
 
